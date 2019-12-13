6ec871f4125899f81bdc29523d3fe331ca6129b1
Geancarlo Chamorro Espinoza <geancarlo.chamorro@anddes.com>
2019-12-13 12:30:12 -0500

    update traer pk_fd del usuario al SCP

--- myscriptScpClientes.sql
+++ myscriptScpClientes.sql
@@ -33,5 +33,11 @@ where t3.cpersona=4551;
 delete from tpersona as t 
 where t.cpersona=4551;
 
+-- Ultimo cambio de pk_flowdesk
+
+ALTER TABLE tpersona ADD COLUMN pk_fd varchar(5);
+update tpersona set pk_fd = 582 where cpersona=4122;
+update tpersona set pk_fd = 615 where cpersona=4118;
+
 
 
